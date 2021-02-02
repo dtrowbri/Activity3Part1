576e1ff9486253885a2a027cbf0cfb3640541e55
Donald_Trowbridge <dtrowbridg@my.gcu.edu>
2021-02-02 00:03:06 -0700

    Add project files.

---
+++ Activity3Part1/Controllers/CustomerController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Activity3Part1.Models;
+
+namespace Activity3Part1.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private List<CustomerModel> customers = new List<CustomerModel> {
+            new CustomerModel(0, "Alan Davies", 55),
+            new CustomerModel(1, "Stephen Fry", 63),
+            new CustomerModel(2, "Sandy Toksvig", 62),
+            new CustomerModel(3, "Rob Brydon", 55),
+            new CustomerModel(4, "Phil Jupitus", 58),
+            new CustomerModel(5, "David Mitchell", 46),
+            new CustomerModel(6, "Bill Bailey", 55)
+         };
+        // GET: Customer
+        public ActionResult Index()
+        {
+            Tuple<List<CustomerModel>, CustomerModel> customerTuple = new Tuple<List<CustomerModel>, CustomerModel>(customers, customers[0]);
+
+            return View("Customer", customerTuple);
+        }
+
+        [HttpPost]
+        public ActionResult onSelectCustomer(string customer)
+        {
+            CustomerModel selectedCustomer = customers[Convert.ToInt32(customer)];
+            Tuple<List<CustomerModel>, CustomerModel> customerTuple = new Tuple<List<CustomerModel>, CustomerModel>(customers, selectedCustomer);
+            return PartialView("_CustomerDetails", customerTuple.Item2);
+        }
+
+    }
+}
---
+++ Activity3Part1/Models/CustomerModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Activity3Part1.Models
+{
+    public class CustomerModel
+    {
+        public CustomerModel(int ID, string Name, int Age)
+        {
+            this.ID = ID;
+            this.Name = Name;
+            this.Age = Age;
+        }
+
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
